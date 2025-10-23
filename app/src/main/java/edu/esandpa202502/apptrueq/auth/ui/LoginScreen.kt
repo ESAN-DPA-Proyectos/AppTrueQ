b9eb1924cd501231a0224f6ed486050d5e33ffce
Ruben Cano <18100439@ue.edu.pe>
2025-10-22 22:11:26 -0500

    feat: Implementar UI básica para las pantallas de Login y Registro
    Se ha añadido la interfaz de usuario inicial para las funcionalidades de autenticación, incluyendo:
    
    -   **LoginScreen:** Un Composable con campos de texto para email y contraseña, y un botón de "Iniciar Sesión".
    -   **RegisterScreen:** Un Composable con campos para nombre, email, contraseña y confirmación de contraseña, junto con un botón de "Registrar".
    -   **Logout:** Se ha creado un archivo vacío `Logout.kt` como placeholder para la futura funcionalidad de cierre de sesión.
    
    Además, se ha limpiado el `MainActivity.kt`, eliminando el código de ejemplo `Greeting` para dejarlo preparado para la implementación de la navegación.

--- app/src/main/java/edu/esandpa202502/apptrueq/auth/ui/LoginScreen.kt
+++ app/src/main/java/edu/esandpa202502/apptrueq/auth/ui/LoginScreen.kt
@@ -1,2 +1,60 @@
 package edu.esandpa202502.apptrueq.auth.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+    ) {
+        Text("Iniciar Sesión", style = MaterialTheme.typography.titleLarge)
+
+        // email
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Correo electrónico") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+        )
+
+        //password
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+        )
+        // dar espacio entre el texto y el boton
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // boton de iniciar sesion
+        Button(onClick = { /*TODO*/ }) {
+            Text("Iniciar Sesión")
+        }
+
+
+    }
+
+}
+
--- app/src/main/java/edu/esandpa202502/apptrueq/auth/ui/RegisterScreen.kt
+++ app/src/main/java/edu/esandpa202502/apptrueq/auth/ui/RegisterScreen.kt
@@ -1,2 +1,73 @@
 package edu.esandpa202502.apptrueq.auth.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+
+fun RegisterScreen(
+    modifier: Modifier = Modifier
+) {
+    var name by remember  { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(16.dp)
+    ){
+        Text(text = "Registro de Usuario", style = MaterialTheme.typography.titleLarge)
+
+        // Campo de texto para el nombre de usuario
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nombre") },
+        )
+
+        // Campo de texto para el correo electrónico
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Correo electrónico") },
+        )
+
+        // Campo de texto para la contraseña
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation(), // Para ocultar la contraseña
+        )
+
+        // Campo de texto para confirmar la contraseña
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirmar contraseña") },
+            visualTransformation = PasswordVisualTransformation(), // Para ocultar la contraseña
+        )
+
+        // Botón para registrar al usuario
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Registrar")
+        }
+
+    }
+
+}
